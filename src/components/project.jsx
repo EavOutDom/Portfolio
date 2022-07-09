d8874ea78d46bbc03ea05f288d18801afd583a02
EavOutDom <eavoutdom400@gmail.com>
2022-07-09 21:59:39 +0700

    update 11

--- src/components/project.jsx
+++ src/components/project.jsx
@@ -1,34 +1,33 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { Link } from "react-router-dom";
-import { motion, useAnimation } from "framer-motion";
-import { useInView } from "react-intersection-observer";
+// import { motion, useAnimation } from "framer-motion";
+// import { useInView } from "react-intersection-observer";
 const Project = ({ id, name, img }) => {
-    const controls = useAnimation();
-    const [ref, inView] = useInView();
-    useEffect(() => {
-        if (inView) {
-            controls.start("visible");
-        }
-    }, [controls, inView]);
-    const animation = {
-        hidden: { x: -200 },
-        visible: {
-            x: 0,
-            transition: {
-                duration: 0.5,
-                type: "spring",
-                bounce: 1,
-                delay: 0.2,
-            },
-        },
-    };
+    // const controls = useAnimation();
+    // const [ref, inView] = useInView();
+
+    // const animation = {
+    //     hidden: { x: -200 },
+    //     visible: {
+    //         x: 0,
+    //         transition: {
+    //             duration: 0.5,
+    //             type: "spring",
+    //             bounce: 1,
+    //             delay: 0.2,
+    //         },
+    //     },
+    // };
+
+    // useEffect(() => {
+    //     if (inView) {
+    //         controls.start("visible");
+    //     }
+    // }, [controls, inView]);
+    
     return (
         <div className="md:hover:scale-[1.05] duration-300">
-        <motion.div
-            ref={ref}
-            initial="hidden"
-            animate={controls}
-            variants={animation}
+        <div
             className={
                 "w-60 h-60 shadow-xl md:hover:shadow-2xl rounded-md my-4 md:hover:scale-[1.1] transition-all duration-300 hover:transition-all hover:duration-300"
             }
@@ -41,7 +40,7 @@ const Project = ({ id, name, img }) => {
                 />
                 <div className={"py-4 text-center font-bold"}>{name}</div>
             </Link>
-        </motion.div>
+        </div>
         </div>
     );
 };
